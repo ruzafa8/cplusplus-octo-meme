34402506e7e30a55ac7ef31154aec9df92d2f237
Alejandro Ruzafa <ruzafa8@gmail.com>
2023-09-17 18:14:38 +0200

    fix(00): compile ex03 and ex04

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -5,8 +5,8 @@
 std::string replace(std::string line, std::string s1, std::string s2) {
     std::string result = "";
     std::string::size_type pos;
-    int i = 0;
-    int s1_len = s1.length();
+    size_t i = 0;
+    size_t s1_len = s1.length();
 
     while (i < line.length()) {
         pos = line.find(s1, i);
@@ -19,8 +19,8 @@ std::string replace(std::string line, std::string s1, std::string s2) {
     return result;
 }
 
-bool validateArgs(int argc, char *argv[]) {
-    if (argc !== 4) {
+bool validateArgs(int argc) {
+    if (argc != 4) {
         std::cout << "usage: ./convert [filename] [string_to_replace] [string_replacement]" << std::endl;
         return false;
     }
@@ -28,20 +28,20 @@ bool validateArgs(int argc, char *argv[]) {
 }
 
 int main(int argc, char *argv[]) {
-    if (!validateArgs(argc, argv)) 
+    if (!validateArgs(argc)) 
         return 1;
 
     std::string filename = argv[1];
     std::string c1 = argv[2];
     std::string c2 = argv[3];
 
-    ifstream iFile(filename);
+    std::ifstream iFile(filename);
     if (!iFile.is_open()) {
         std::cout << "Error: could not open input file" << std::endl;
         return 1;
     }
 
-    ofstream oFile(filename + ".replace");
+    std::ofstream oFile(filename + ".replace");
     if (!oFile.is_open()) {
         std::cout << "Error: could not open output file" << std::endl;
         iFile.close();
@@ -49,8 +49,8 @@ int main(int argc, char *argv[]) {
     }
 
     std::string line;
-    while (getline(file, line)) {
-        output << replace(line) << std::endl;
+    while (std::getline(iFile, line)) {
+        oFile << replace(line, c1, c2) << std::endl;
     }
     iFile.close();
     oFile.close();
