8f9b6b226422bc6b80aec79246d9c9c7fe485d06
Alejandro Ruzafa <ruzafa8@gmail.com>
2023-09-18 21:14:38 +0200

    fix(01): ex03 compilation error

--- module01/ex03/Weapon.hpp
+++ module01/ex03/Weapon.hpp
@@ -10,7 +10,7 @@ class Weapon {
 		Weapon(std::string type);
         ~Weapon(void);
 
-        std::string getType(void) const;
+        const std::string &getType(void);
         void setType(std::string type);
 
     private:
